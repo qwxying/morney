bd19f9bff823c485edcf5a598584f83d46720a2c
qwxying <qwxying@gmail.com>
2020-05-19 18:52:06 +0800

    完成导航栏

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,16 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
     </router-link>
-    |
-    <router-link to="/labels" class="item">
+
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="label"/>
       标签
     </router-link>
-    |
-    <router-link to="/statistics" class="item">
+
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -27,17 +27,24 @@
   nav {
     display: flex;
     box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
-    > .item{
+    flex-direction: row;
+    /*font-size: 12px;*/
+    > .item {
       padding: 2px 0;
       width: 33.33333333%;
       display: flex;
       justify-content: center;
       align-items: center;
       flex-direction: column;
-      .icon{
+
+      .icon {
         width: 32px;
         height: 32px;
       }
+    }
+
+    > .item.selected {
+      color: #2f3cf4;
     }
   }
 </style>
