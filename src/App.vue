c017e62bbb5faf350cc51017aab8d7752413cc01
qwxying <qwxying@gmail.com>
2020-05-19 18:20:29 +0800

    三个小图标

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="">
+  <div id="app">
     <router-view/>
   </div>
 </template>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
+const path = require("path")
+// import path from 'path'
+// import mp from 'svg-sprite-loader-mod/plugin'
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack: config => {
+    const dir = path.resolve(__dirname, "src/assets/icons")
+
+    config.module
+      .rule("svg-sprite")
+      .test(/\.(svg)(\?.*)?$/)
+      .include.add(dir).end()
+      .use("svg-sprite-loader-mod").loader("svg-sprite-loader-mod").options({extract: false})
+      .tap(options => ({...options, plugins: [{removeAttrs: {attrs: "fill"}}]}))
+      .end()
+    config.plugin("svg-sprite").use(require("svg-sprite-loader-mod/plugin"), [{plainSprite: true}])
+    config.module.rule("svg").exclude.add(dir)
+  }
 }
