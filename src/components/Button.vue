5ec2bc089001e2941dbe57d2e0c990779f702102
qwxying <qwxying@gmail.com>
2020-05-22 16:11:56 +0800

    改造标签编辑页面的CSS，提取了几个通用组件

---
+++ src/components/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="button"
+          @click="$emit('click',$event)">
+    <slot/>
+  </button>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Button'
+  };
+</script>
+
+<style lang="scss" scoped>
+  .button {
+    background: #767676;
+    color: white;
+    border-radius: 4px;
+    border: none;
+    height: 32px;
+    padding: 0 8px;
+
+    &-wrapper {
+      text-align: center;
+      padding: 8px;
+      margin-top: 8px;
+    }
+  }
+</style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,12 @@
 <template>
   <Layout class-prefix="layout">
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
-    <Notes field-name="备注" placeholder="在此输入备注" @update:value="onUpdateNotes"/>
+    <div class="notes">
+      <FormItem field-name="备注"
+                placeholder="在此输入备注"
+                @update:value="onUpdateNotes"
+      />
+    </div>
     <Types :value.sync="record.type"/>
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
   </Layout>
@@ -10,8 +15,8 @@
 <script lang="ts">
   import Vue from 'vue';
   import Tags from '@/components/Money/Tags.vue';
-  import Notes from '@/components/Money/Notes.vue';
   import Types from '@/components/Money/Types.vue';
+  import FormItem from '@/components/Money/FormItem.vue';
   import NumberPad from '@/components/Money/NumberPad.vue';
   import {Component, Watch} from 'vue-property-decorator';
   import recordListModel from '@/models/recordListModel';
@@ -20,7 +25,7 @@
   const recordList = recordListModel.fetch();
   const tagList = tagListModel.fetch();
 
-  @Component({components: {Types, Notes, Tags, NumberPad}})
+  @Component({components: {FormItem, Types, Tags, NumberPad}})
   export default class Money extends Vue {
     tags = tagList;
     recordList = recordList;
@@ -52,6 +57,10 @@
     display: flex;
     flex-direction: column;
     justify-content: flex-end;
+  }
+
+  .notes {
+    padding: 12px 0;
   }
 </style>
 
