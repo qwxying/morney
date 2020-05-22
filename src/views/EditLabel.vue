42dccbc0475103376c3717c273373940c517b725
qwxying <qwxying@gmail.com>
2020-05-22 10:06:04 +0800

    将tag的string改为{id, name}

---
+++ src/views/EditLabel.vue
@@ -0,0 +1,15 @@
+<template>
+  <Layout>
+    编辑标签
+  </Layout>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'EditLabel'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
