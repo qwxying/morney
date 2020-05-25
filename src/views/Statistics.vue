1140a96653525f0644a44cf6de1b29b5864e2a4a
qwxying <qwxying@gmail.com>
2020-05-25 12:00:54 +0800

    完成统计列表的排序

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -7,7 +7,7 @@
           :data-source="intervalList"
           :value.sync="interval"/>
     <ol>
-      <li v-for="group in result" :key="group.title">
+      <li v-for="(group, index) in groupedList" :key="index">
         <h3 class="title">{{beautify(group.title)}}</h3>
         <ol>
           <li v-for="item in group.items" :key="item.id" class="record">
@@ -29,6 +29,7 @@
   import intervalList from '@/constants/intervalList';
   import recordTypeList from '@/constants/recordTypeList';
   import dayjs from 'dayjs';
+  import clone from '@/lib/clone';
 
 
   @Component({
@@ -51,16 +52,21 @@
       return (this.$store.state as RootState).recordList;
     }
 
-    get result() {
+    get groupedList() {
       const {recordList} = this;
-      type HashTableValue = { title: string; items: RecordItem[] }
-      const hashTable: { [key: string]: HashTableValue } = {};
-      for (let i = 0; i < this.recordList.length; i++) {
-        const [date, time] = recordList[i].createdAt!.split('T');
-        hashTable[date] = hashTable[date] || {title: date, items: []};
-        hashTable[date].items.push(recordList[i]);
+      if (recordList.length === 0) return [];
+      const newList = clone(recordList).sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf());
+      const result = [{title: dayjs(newList[0].createdAt).format('YYYY-MM-DD'), items: [newList[0]]}];
+      for (let i = 0; i < newList.length; i++) {
+        const current = newList[i];
+        const last = result[result.length - 1];
+        if (dayjs(last.title).isSame(dayjs(current.createdAt), 'day')) {
+          last.items.push(current);
+        } else {
+          result.push({title: dayjs(current.createdAt).format('YYYY-MM-DD'), items: [current]});
+        }
       }
-      return hashTable;
+      return result;
     }
 
     beforeCreate() {
